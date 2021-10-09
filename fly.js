12ce58f7d641c579360eba8fb650cdce2b844559
Jyotsna <guptajyotsna2002@gmail.com>
2021-10-09 11:53:07 +0530

    gravity added

--- fly.js
+++ fly.js
@@ -1,11 +1,23 @@
 var character=document.getElementById("bird");
 var jumping=0;
 var counter=0;
+function free()
+{
 setInterval(down,20);
 function down()
 {
     var charactop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top=(charactop+3)+'px'; 
+    character.style.top=(charactop+4)+'px'; 
+   /*var holetop=parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var blockleft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    */
+    var ctop=window.getComputedStyle(character).getPropertyValue("top");
+    if(ctop==470)  
+    {
+        window.alert("game over!!");
+        character.style.top=100+"px";
+    } 
+}
 }
 function jump(){
     jumping= 1;
@@ -31,10 +43,10 @@ function startGame()
     block.style.visibility="visible";
     var i=document.getElementsByClassName("outer")[0].offsetWidth;
     var x=7+Math.random()*10;
-    var y=8+Math.random()*10
+    var y=8+Math.random()*10;
     document.getElementById("pipetop").style.top ="-"+x+"%";
     document.getElementById("pipebottom").style.bottom="-"+y+"%";
-    id = setInterval(moveBlock,5)
+    id = setInterval(moveBlock,5);
     function moveBlock()
     {
         i--;
